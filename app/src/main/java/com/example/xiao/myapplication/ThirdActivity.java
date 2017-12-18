c5dd47551c104c6fdf40edf997098b11015e8f34
xiao <709008281@qq.com>
2017-12-18 23:27:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiao/myapplication/ThirdActivity.java
@@ -0,0 +1,15 @@
+package com.example.xiao.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by xiao on 2017/12/18.
+ */
+
+public class ThirdActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
