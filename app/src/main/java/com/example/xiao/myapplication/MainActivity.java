c58d97e3ed2fa2eae007294d1448637e28144c3a
xiao <709008281@qq.com>
2017-12-19 23:02:43 +0800

    init

--- app/src/main/java/com/example/xiao/myapplication/MainActivity.java
+++ app/src/main/java/com/example/xiao/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void onClick(View view){
+    public void onClick_but1(View view){
         Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+        Toast.makeText(this,"你点击了按钮",Toast.LENGTH_SHORT).show();
         startActivity(intent);
     }
 }
