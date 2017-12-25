76d6dc4f8d93d223c2548477d12b09ced116d88d
xiao <709008281@qq.com>
2017-12-25 21:09:36 +0800

    init

--- app/src/main/java/com/example/xiao/myapplication/MainActivity.java
+++ app/src/main/java/com/example/xiao/myapplication/MainActivity.java
@@ -1,21 +1,66 @@
 package com.example.xiao.myapplication;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends Activity implements DialogInterface.OnClickListener {
+    //定义显示接收Activity传入值的TextView
+    private TextView tv_main_result = null;
+    //定义跳转的Button按钮
+    private Button btn_main_skip = null;
+    //定义一个startActivityForResult（）方法用到的整型值
+    private final int requestCode = 1500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
-    public void onClick_but1(View view){
-        Intent intent=new Intent(MainActivity.this,SecondActivity.class);
-        Toast.makeText(this,"你点击了按钮",Toast.LENGTH_SHORT).show();
-        startActivity(intent);
+    public void initView(){
+        tv_main_result = (TextView)findViewById(R.id.tv_main_result);
+        btn_main_skip = (Button)findViewById(R.id.btn_main_skip);
+        btn_main_skip.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                //采用Intent普通传值的方式
+                intent.putExtra("skip", "我是MainActivity传过来的值！");
+                //跳转Activity
+                startActivityForResult(intent, requestCode);
+
+            }
+        });
+    }
+
+
+    /**
+     * 接收当前Activity跳转后，目标Activity关闭后的回传值
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(resultCode){
+            case RESULT_OK:{//接收并显示Activity传过来的值
+                Bundle bundle = data.getExtras();
+                String rs = bundle.getString("rs");
+                tv_main_result.setText(rs);
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+
     }
 }
--- app/src/main/java/com/example/xiao/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/xiao/myapplication/SecondActivity.java
@@ -1,21 +1,50 @@
 package com.example.xiao.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by xiao on 2017/12/18.
  */
 
 public class SecondActivity extends Activity {
-    Button bt1;
-    View v1;
+    //定义显示接收Activity传入值的TextView
+    private TextView tv_rs_result = null;
+    //定义跳转的Button按钮
+    private Button btn_rs_skip = null;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        v1=getLayoutInflater().inflate(R.layout.activity_second,null);
+        setContentView(R.layout.activity_second);
+        initView();
+    }
+    private void initView(){
+        //接收Activity传过来的值
+        final Intent data = getIntent();
+        tv_rs_result = (TextView)findViewById(R.id.tv_rs_result);
+        //将Activity传过来的值赋给textview显示
+        tv_rs_result.setText(data.getStringExtra("skip"));
 
+        btn_rs_skip = (Button)findViewById(R.id.btn_rs_skip);
+        btn_rs_skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {//采用Intent绑定Bundle的形式回传值
+
+                //新建一个Bundle，Bundle主要放值类型
+                Bundle bundle = new Bundle();
+                bundle.putString("rs", "我是RsActivity关闭后回传的值！");
+                //将Bundle赋给Intent
+                data.putExtras(bundle);
+                //跳转回MainActivity
+                //注意下面的RESULT_OK常量要与回传接收的Activity中onActivityResult（）方法一致
+                SecondActivity.this.setResult(RESULT_OK, data);
+                //关闭当前activity
+               SecondActivity.this.finish();
+            }
+        });
     }
 }
